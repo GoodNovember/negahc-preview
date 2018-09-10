017d5b11aa766595285eb6b90f4ee79b374a1a3e
Victor Lawrence <VictorLawrenceCode@gmail.com>
2018-09-10 14:50:17 -0400

    Filled up the docs folder with stuff that is not documentation related.

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const path = require('path')
 
 const PORT = 8080
 
+const PUBLIC_SERVER_FOLDER = './docs'
+
 const bundler = new Bundler(path.join(__dirname, '/src/index.html'), {
-	outDir:'./public'
+	outDir:PUBLIC_SERVER_FOLDER,
+	publicUrl:'./'
 })
 
 // use the media from the source:
 // app.use('/media',express.static(path.join(__dirname,'/src/media')))
 // use the compiled data here:
-app.use(express.static(path.join(__dirname,'/public')))
+app.use(express.static(path.join(__dirname, PUBLIC_SERVER_FOLDER)))
 // use the parcel bundler.
 app.use(bundler.middleware())
 
